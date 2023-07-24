e1f97f1f119ac05f13a88852c1d0d865f9884ed2
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2023-07-23 22:39:51 -0300

    feat: insert count products in list order

--- backend/src/order/order.service.ts
+++ backend/src/order/order.service.ts
@@ -93,25 +93,24 @@ export class OrderService {
             throw new NotFoundException('Orders not found');
         }
 
-        // const ordersProduct =
-        //     await this.orderProductService.findAmountProductsByOrderId(
-        //         orders.map((order) => order.id),
-        //     );
+        const ordersProduct =
+            await this.orderProductService.findAmountProductsByOrderId(
+                orders.map((order) => order.id),
+            );
 
-        // return orders.map((order) => {
-        //     const orderProduct = ordersProduct.find(
-        //         (currentOrder) => currentOrder.order_id === order.id,
-        //     );
+        return orders.map((order) => {
+            const orderProduct = ordersProduct.find(
+                (currentOrder) => currentOrder.order_id === order.id,
+            );
 
-        //     if (orderProduct) {
-        //         return {
-        //             ...order,
-        //             amountProducts: Number(orderProduct.total),
-        //         };
-        //     }
-        //     return order;
-        // });
+            if (orderProduct) {
+                return {
+                    ...order,
+                    amountProducts: Number(orderProduct.total),
+                };
+            }
+            return order;
+        });
 
-        return orders
     }
 }
